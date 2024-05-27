700f3adf54fbd84b1d92549b4f9036e698b7a03e
smokydoky <dimitris.kafs@gmail.com>
2024-05-27 18:23:58 -0400

    fixes and improvements

--- phishing_url_checker/main.py
+++ phishing_url_checker/main.py
@@ -7,7 +7,7 @@ def phishing_url_check(url):
         r"\d{1,3}-\d{1,3}-\d{1,3}-\d{1,3}",  # IP in the domain part
         r"(login|verify|account|secure|update|auth)\.",  # Common phishing words
         r"(\.com-|\.net-|\.org-|\.ca-|\.ru-|\.info-|\.co\.uk-)",  # Continuing after top-level domain
-        r"([a-zA-Z0-9-]+\.)+\d{1,3}",  # Domain name ending in numbers
+        r"https?://[a-zA-Z0-9-.]*[a-zA-Z]\d+[\.\w]*/?",  # Domain name ending in numbers
         r"https?://[a-zA-Z0-9-.]+[a-zA-Z0-9-]{60,}",  # Long domain names
     ]
 
@@ -21,10 +21,15 @@ def phishing_url_check(url):
 # Testing List
 test_url_list = [
     "http://www.google.com-verify-account.secure.com",
-    "http://192-168-1-1-secure.com",
+    "http://192-172-1-1-secure.com",
     "http://update-facebook-info.com",
     "http://bankofamerica.secure-login.verify-credentials.com",
     "http://safe.com",
+    "http://example123.com",
+    "http://example.com123/",
+    "http://example.com",
+    "https://subdomain.example456.net/",
+    "http://example789.com/a/path",
 ]
 
 manualinput = input("Do you want to give a url? (y/n) ")
